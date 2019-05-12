e1fb7981d22915259a17b304ff5ce6b7fd2ae985
Antoine Albertelli <antoinealb@google.com>
2019-05-12 19:44:12 +0200

    Convert parameter_flash_storage

--- lib/parameter/tests/parameter_test.cpp
+++ lib/parameter/tests/parameter_test.cpp
@@ -3,29 +3,6 @@
 #include "parameter/parameter_port.h"
 #include <cstdlib>
 
-void parameter_port_lock(void)
-{
-}
-
-void parameter_port_unlock(void)
-{
-}
-
-void parameter_port_assert(int condition)
-{
-    CHECK_TRUE(condition);
-}
-
-void *parameter_port_buffer_alloc(size_t size)
-{
-    return malloc(size);
-}
-
-void parameter_port_buffer_free(void *buffer)
-{
-    free(buffer);
-}
-
 TEST_GROUP(ParameterNamespaceInit)
 {
     parameter_namespace_t rootns;
